298c2f496f4df8f5d87c44aeeaad8638e1a9165b
Alejandro Basso <alejandro.basso.abad.dam@gmail.com>
2022-04-04 07:46:15 -0400

    Chef show action with tests

---
+++ test/integration/chefs_show_test.rb
@@ -0,0 +1,20 @@
+require "test_helper"
+
+class ChefsShowTest < ActionDispatch::IntegrationTest
+  def setup
+    @chef = Chef.create!(chef_name: "alejandro", email: "alejandro@correo.es", password: "password", password_confirmation: "password")
+    @recipe = Recipe.create(name: "macarrones", description: "con tomatico", chef: @chef)
+    @recipe2 = @chef.recipes.build(name: "ensalada", description: "ensalada de tomate")
+    @recipe2.save
+  end
+
+  test "should get chefs show" do
+    get chef_path(@chef)
+    assert_template "chefs/show"
+    assert_select "a[href=?]", recipe_path(@recipe), text: @recipe.name
+    assert_select "a[href=?]", recipe_path(@recipe2), text: @recipe2.name
+    assert_match @recipe.description, response.body
+    assert_match @recipe2.description, response.body
+    assert_match @chef.chef_name, response.body
+  end
+end
