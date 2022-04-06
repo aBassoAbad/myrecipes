700fc15b0a00aa5c1caa1d1b4c5d4f52f7d18310
Alejandro Basso <alejandro.basso.abad.dam@gmail.com>
2022-04-06 10:20:25 -0400

    Complete authentication system

--- test/integration/chefs_edit_test.rb
+++ test/integration/chefs_edit_test.rb
@@ -6,6 +6,14 @@ class ChefsEditTest < ActionDispatch::IntegrationTest
                            email: "alejandro@correo.com",
                                password: "password", 
                                password_confirmation: "password")
+        @chef2 = Chef.create!(chefname: "alejandro2", 
+                            email: "alejandro2@correo.com",
+                                password: "password", 
+                                password_confirmation: "password")
+        @admin = Chef.create!(chefname: "alejandro3", 
+                            email: "alejandro3@correo.com",
+                                password: "password", 
+                                password_confirmation: "password", admin: true)
        end
        
        test "reject an invalid edit" do
@@ -16,7 +24,8 @@ class ChefsEditTest < ActionDispatch::IntegrationTest
          assert_template 'chefs/edit'
        end
        
-       test "accept valid signup" do
+       test "accept valid edit" do
+         sing_in_as(@chef)
          get edit_chef_path(@chef)
          assert_template 'chefs/edit'
          patch chef_path(@chef), params: { chef: { chefname: "alejandro1", 
@@ -26,5 +35,31 @@ class ChefsEditTest < ActionDispatch::IntegrationTest
          @chef.reload
          assert_match "alejandro1", @chef.chefname
          assert_match "alejandro1@correo.com", @chef.email
+       end
+
+       test "accept edit attempt by admin user" do
+        sing_in_as(@admin)
+        get edit_chef_path(@chef)
+        assert_template 'chefs/edit'
+        patch chef_path(@chef), params: { chef: { chefname: "alejandro5", 
+                                    email: "alejandro5@correo.com" } }
+        assert_redirected_to @chef
+        assert_not flash.empty?
+        @chef.reload
+        assert_match "alejandro5", @chef.chefname
+        assert_match "alejandro5@correo.com", @chef.email
+       end
+
+       test "redirect edit attemp by another non-admin user" do
+        sing_in_as(@chef2)
+        updated_name = "alejandro6"
+        updated_email = "alejandro6@correo.com"
+        patch chef_path(@chef), params: { chef: { chefname: updated_name, 
+                                    email: updated_email } }
+        assert_redirected_to chefs_path
+        assert_not flash.empty?
+        @chef.reload
+        assert_match "alejandro", @chef.chefname
+        assert_match "alejandro@correo.com", @chef.email
        end
 end
