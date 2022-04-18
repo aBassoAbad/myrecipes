a9f10139335caca59726d7b06905663651b6cde0
Alejandro Basso <alejandro.basso.abad.dam@gmail.com>
2022-04-18 06:59:41 -0400

    ActionCable en produccion

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,6 +43,8 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
+  config.action_cable.allowed_request_origins = [
+    'https://alejandro-myrecipes.herokuapp.com', 'http://alejandro-myrecipes.herokuapp.com']
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
@@ -117,4 +119,5 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  
 end
