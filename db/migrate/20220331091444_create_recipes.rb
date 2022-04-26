7783a4e3884b94f4f79e33b77405debe77c407e4
alejandro.basso.abad.dam <alejandro.basso.abad.dam@gmail.com>
2022-04-26 02:58:34 -0400

    Cambiadas migraciones

--- db/migrate/20220331091444_create_recipes.rb
+++ db/migrate/20220331091444_create_recipes.rb
@@ -2,7 +2,7 @@ class CreateRecipes < ActiveRecord::Migration[6.1]
   def change
     create_table :recipes do |t|
         t.string :name
-        t.string :email
+        t.string :description
         t.timestamps
     end
   end
